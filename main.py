66be2eebc2d95ecc022ef17c20879c519203861a
Кучумов Вячеслав <Kuchumov76@mail.ru>
2025-12-17 15:11:20 +0500

    Create new release with this object

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webelement import WebElement
 import time
 
 import random
@@ -18,6 +19,15 @@ wait = WebDriverWait(driver, 20)
 
 def getClientElement():
     return EC.presence_of_element_located((By.ID, "ru.global-system.gl3.client"))
+
+def getModalWindow():
+    return EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'gwindow-modal')]"))
+
+def generateObjectName():
+    return f"TEST_OBJECT_{random.randint(0,100)}{random.randint(0,100)}{random.randint(0,100)}"
+
+def generateReleaseName():
+    return f"TEST_RELEASE_{random.randint(0,100)}{random.randint(0,100)}{random.randint(0,100)}"
 
 def login():
     user_input = wait.until(
@@ -35,46 +45,55 @@ def goToPage(url):
     body.send_keys(Keys.RETURN)
     wait.until(getClientElement())
 
-def generateObjectName():
-    return f"TEST_OBJECT{random.randint(0,100)}{random.randint(0,100)}{random.randint(0,100)}"
 
 class ToolBarBtn:
     EDIT_IN_LIST = "(//div[contains(@class, 'PToolButton')])[10]"
     INSERT = "(//div[contains(@class, 'PToolButton')])[7]"
-    
-class HotKey:
-    SAVE_FORM = Keys.LEFT_CONTROL + "s"
-    INFO = Keys.LEFT_CONTROL + Keys.ALT + "i"
-    EXIT = Keys.ESCAPE
+    CREATE_RELEASE = "//div[contains(text(), 'Создать релиз')]"
 
 def click(button):
     match button:
         case ToolBarBtn.EDIT_IN_LIST:
             btn = wait.until(
-                EC.element_to_be_clickable((By.XPATH, ToolBarBtn.EDIT_IN_LIST))
+                EC.element_to_be_clickable((By.XPATH, button))
             )
             btn.click()
         case ToolBarBtn.INSERT:
             btn = wait.until(
-                EC.element_to_be_clickable((By.XPATH, ToolBarBtn.INSERT))
+                EC.element_to_be_clickable((By.XPATH, button))
             )
+            btn.click()
+        case ToolBarBtn.CREATE_RELEASE:
+            btn = wait.until(
+                EC.element_to_be_clickable((By.XPATH, button))
+            )
+            btn.click()
             btn.click()
         case _ :
             pass
+
+class HotKey:
+    SAVE_FORM = Keys.LEFT_CONTROL + "s"
+    INFO = Keys.LEFT_CONTROL + Keys.ALT + "i"
+    EXIT = Keys.ESCAPE
+    CHOOSE_MODAL = Keys.F2
+
 def send_hot_key(key_combination):
     wait.until(getClientElement())
     body = wait.until(
         EC.element_to_be_clickable((By.TAG_NAME, "body"))
     )
+    body.click()
     body.send_keys(key_combination)
 
-def enter_text(class_name, text):
+def enter_text(element: WebElement, text):
     wait.until(getClientElement())
-    text_field = wait.until(
-        EC.element_to_be_clickable((By.XPATH, f"(//input[@class='{class_name}'])[3]"))
-    )
-    text_field.click()
-    text_field.send_keys(text)
+    element.click()
+    element.send_keys(text)
+def clear_text(element: WebElement):
+    wait.until(getClientElement())
+    element.click()
+    element.send_keys(Keys.LEFT_CONTROL+ "a" + Keys.BACKSPACE)
         
 
 try:
@@ -87,11 +106,30 @@ try:
 
     click(ToolBarBtn.INSERT)
 
-
-    enter_text("D56M6YB-t-c D56M6YB-t-h", generateObjectName())
+    system_name_field = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "(//input[@class='D56M6YB-t-c D56M6YB-t-h'])[3]"))
+    )
+    enter_text(system_name_field, generateObjectName())
     send_hot_key(HotKey.SAVE_FORM)
-    
-    send_hot_key(HotKey.INFO)
+
+    wait.until(getModalWindow())
+
+    click(ToolBarBtn.CREATE_RELEASE)
+
+    release_name_field = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "(//input[contains(@value, 'Перенос конфигурации')])"))
+    )
+
+    clear_text(release_name_field)
+    enter_text(release_name_field, generateReleaseName())
+    send_hot_key(Keys.ENTER)
+
+    send_hot_key(HotKey.CHOOSE_MODAL)
+    send_hot_key(HotKey.CHOOSE_MODAL)
+
+    send_hot_key(HotKey.CHOOSE_MODAL)
+
+    send_hot_key(HotKey.SAVE_FORM)
     # goToPage("/PGDEV/Btk_ConfiguratorMainMenu/gtk-ru.bitec.app.btk.Btk_SettingGroup%23List/")
     
     
