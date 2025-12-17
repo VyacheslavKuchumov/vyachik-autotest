fa426a88a7489dc78e0fe70419523c43d9db7e24
Кучумов Вячеслав <Kuchumov76@mail.ru>
2025-12-17 14:09:29 +0500

    add creation of an object

--- main.py
+++ main.py
@@ -5,36 +5,47 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+import random
+
 BASE_URL = "http://localhost:8080"
 
-class ToolBarBtn:
-    EDIT_IN_LIST = "(//div[contains(@class, 'PToolButton')])[10]"
-    INSERT = "(//div[contains(@class, 'PToolButton')])[7]"
+
 
 # Setup
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 20)
 # driver.maximize_window()  # Maximize window
 
-def get_client_element():
+def getClientElement():
     return EC.presence_of_element_located((By.ID, "ru.global-system.gl3.client"))
-    
 
 def login():
     user_input = wait.until(
         EC.element_to_be_clickable((By.NAME, "j_username"))
     )
     user_input.send_keys(Keys.RETURN)
-    wait.until(get_client_element())
+    wait.until(getClientElement())
 
 def goToPage(url):
     driver.get(BASE_URL + url)
-    wait.until(get_client_element())
+    wait.until(getClientElement())
     body = wait.until(
         EC.element_to_be_clickable((By.TAG_NAME, "body"))
     )
     body.send_keys(Keys.RETURN)
-    wait.until(get_client_element())
+    wait.until(getClientElement())
+
+def generateObjectName():
+    return f"TEST_OBJECT{random.randint(0,100)}{random.randint(0,100)}{random.randint(0,100)}"
+
+class ToolBarBtn:
+    EDIT_IN_LIST = "(//div[contains(@class, 'PToolButton')])[10]"
+    INSERT = "(//div[contains(@class, 'PToolButton')])[7]"
+    
+class HotKey:
+    SAVE_FORM = Keys.LEFT_CONTROL + "s"
+    INFO = Keys.LEFT_CONTROL + Keys.ALT + "i"
+    EXIT = Keys.ESCAPE
 
 def click(button):
     match button:
@@ -50,6 +61,20 @@ def click(button):
             btn.click()
         case _ :
             pass
+def send_hot_key(key_combination):
+    wait.until(getClientElement())
+    body = wait.until(
+        EC.element_to_be_clickable((By.TAG_NAME, "body"))
+    )
+    body.send_keys(key_combination)
+
+def enter_text(class_name, text):
+    wait.until(getClientElement())
+    text_field = wait.until(
+        EC.element_to_be_clickable((By.XPATH, f"(//input[@class='{class_name}'])[3]"))
+    )
+    text_field.click()
+    text_field.send_keys(text)
         
 
 try:
@@ -60,9 +85,14 @@ try:
     
     goToPage("/PGDEV/Btk_ConfiguratorMainMenu/gtk-ru.bitec.app.btk.Btk_SettingGroup%23List/")
 
-    click(ToolBarBtn.EDIT_IN_LIST)
     click(ToolBarBtn.INSERT)
 
+
+    enter_text("D56M6YB-t-c D56M6YB-t-h", generateObjectName())
+    send_hot_key(HotKey.SAVE_FORM)
+    
+    send_hot_key(HotKey.INFO)
+    # goToPage("/PGDEV/Btk_ConfiguratorMainMenu/gtk-ru.bitec.app.btk.Btk_SettingGroup%23List/")
     
     
 except Exception as e:
